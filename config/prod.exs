b8183e938c754919e13811d1aaa83c47010f02bb
Jacob Lehman <jlehman@ufl.edu>
2017-07-15 19:22:53 -0400

    Test for branch stuff

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :pirates, Pirates.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "pirates-branchtest.herokuapp.com", port: 443],
+  url: [scheme: "https", host: "pirates-branch-jb.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json"
 
